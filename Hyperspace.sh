eb7e36deeca4cb6b3393333219f54584c1dff8f5
RE4869 <139995415+RE4869@users.noreply.github.com>
2025-04-12 23:47:36 +0800

    Update Hyperspace.sh

--- Hyperspace.sh
+++ Hyperspace.sh
@@ -20,7 +20,7 @@ function main_menu() {
     while true; do
         clear
         echo "Hyper0.5B模型"
-        echo "模型：bartowski/Qwen2.5-Coder-0.5B-Instruct-Q8_0.gguf"
+        echo "模型：QuantFactory/Qwen2-0.5B-GGUF/qwen2-0.5b.q8_0.gguf"
         echo "================================================================"
         echo "退出脚本，请按键盘 ctrl + C 退出即可"
         echo "请选择要执行的操作:"
@@ -136,7 +136,7 @@ function deploy_hyperspace_node() {
     sleep 5
 
     # 定义模型变量
-    model="hf:bartowski/Qwen2-0.5B-Instruct-Q8_0.gguf"
+    model="hf:QuantFactory/Qwen2-0.5B-GGUF/qwen2-0.5b.q8_0.gguf"
 
     # 添加模型并重试
     echo "正在通过命令 '/root/.aios/aios-cli models add' 添加模型..."
